b64a8615e316b7c8ca0f46043c7691a33f9ff9a8
Michael B. Young <mikebyoung80@comcast.net>
2018-03-23 05:39:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ if (name === "Susan") {
    console.log("Expected the name to be Susan")
 }
 if (height === "74") {
-    console.log("The name is correct")
+    console.log("The height is correct")
 } else {
-   console.log("Expected the name to be Susan")
+   console.log("Expected height to be 74")
 }
 // Don't worry about this
 module.exports = { name, height
