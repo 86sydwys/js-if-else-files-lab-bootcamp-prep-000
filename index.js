48f8428c908efa2ae3bacd09e7d46be0233bdffe
Michael B. Young <mikebyoung80@comcast.net>
2018-03-23 05:38:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (name === "Susan") {
 } else {
    console.log("Expected the name to be Susan")
 }
-if (height === "Susan") {
+if (height === "74") {
     console.log("The name is correct")
 } else {
    console.log("Expected the name to be Susan")
